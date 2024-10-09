d3b6f59a6721c41613db474e3ca65bdd4c47daa8
vale99-eng <valeria.mendoza@netec.com>
2024-10-09 17:48:50 +0000

    Carga inicial del proyecto app-amplify

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css'; // Asegúrate de importar el archivo CSS
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Despliegue de React con AWS Amplify</h1>
+        <p>¡Felicidades! haz desplegado una aplicación React básica en AWS Amplify.</p>
+        <button className="btn-primary">Aprender más</button>
+      </header>
+    </div>
+  );
+}
+
+export default App;
